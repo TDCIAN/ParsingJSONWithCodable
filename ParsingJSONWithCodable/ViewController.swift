6a5f2a4844aba44f6039340e9d58f8e9a8d63ff8
KJM <tdcian@hexlant.com>
2020-05-10 18:39:34 +0900

    Initial Commit

---
+++ ParsingJSONWithCodable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParsingJSONWithCodable
+//
+//  Created by hexlant_01 on 2020/05/10.
+//  Copyright © 2020 hexlant_01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
